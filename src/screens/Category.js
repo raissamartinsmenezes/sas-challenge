b77c040470f7469818e2a8e841e711e37db30aa3
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-07 23:26:02 -0300

    feat(category): connect component to store

--- src/screens/Category.js
+++ src/screens/Category.js
@@ -1,27 +1,31 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
+import { useSelector, useDispatch } from 'react-redux';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar, Dimensions } from 'react-native';
 
 // import Header from '../components/Header'
 import Button from '../components/Button';
 
-const categoriesMock = [
-  { id: 23, category: 'Historia' },
-  { id: 22, category: 'Geografia' },
-  { id: 20, category: 'Mitologia' },
-  { id: 21, category: 'Esportes' },
-  { id: 24, category: 'Politica' },
-  { id: 9, category: 'Conhecimentos gerais' },
-]
+import getCategories from '../service/api';
+import { changeFirstCategoryName } from '../store/slicers/categoriesSlice';
 
 const Category = ({ navigation }) => {
+  const categories = useSelector(state => state.categories)
+  const dispatch = useDispatch()
+
+  const changeName = () => {
+    alert(JSON.stringify(changeFirstCategoryName('apple')))
+    dispatch(changeFirstCategoryName('apple'))
+  }
+
   return (
       <SafeAreaView>
           <StatusBar translucent barStyle="dark-content"/>
           {/* <Header title="Dev Mobile"/> */}
           <Text style={styles.title}>Categorias</Text>
           <View style={styles.buttons}>
-            {categoriesMock.map((c) => (
-              <Button onClick={() => navigation.navigate('Quizz')} key={c.id} label={c.category}></Button>
+            {categories.map((c) => (
+              <Button onClick={changeName} key={c.id} label={c.category}></Button>
+              // <Button onClick={() => navigation.navigate('Quizz')} key={c.id} label={c.category}></Button>
             ))}
           </View>
       </SafeAreaView>
