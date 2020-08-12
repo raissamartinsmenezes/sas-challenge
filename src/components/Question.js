39d460e4ad7fafd29a4f3bd490acab3233488d1f
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-11 22:01:04 -0300

    feat(question):  add description and answer to question component
    - add selectedAnswer local state
    - add onSelectAnswer function
    - add isCorrectAnswer logic

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, ScrollView } from "react-native";
+import Answer from "./Answer";
 
-const Question = (props) => {
+const Question = ({ question, onSelectAnswer }) => {
+  const { description, answers = [], correctAnswer } = question;
+  const [selectedAnswer, setSelectedAnswer] = useState();
   return (
     <>
-      <Text style={styles.text}>{props.question}</Text>
+      <Text style={styles.text}>{description}</Text>
+      <ScrollView>
+        {answers.map((answer) => (
+          <Answer
+            answer={answer}
+            isSelected={selectedAnswer === answer}
+            onClick={() => {
+              // set answer for inner state purposes
+              setSelectedAnswer(answer);
+              // call parent component's callback
+              const isCorrectAnswer = correctAnswer === answer;
+              onSelectAnswer(isCorrectAnswer);
+            }}
+          />
+        ))}
+      </ScrollView>
     </>
   );
 };
 
-const styles = StyleSheet.create({ 
+const styles = StyleSheet.create({
   text: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
     marginVertical: 30,
     paddingHorizontal: 16,
     lineHeight: 22,
-  }
+  },
 });
 
 export default Question;
