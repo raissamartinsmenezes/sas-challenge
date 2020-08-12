7dfdee572fd43b0af7f766fb2b87159e36d9350a
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-11 22:01:45 -0300

    feat(store): update combine reducers

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,12 +2,10 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit"
 
 import categories from "./slices/categoriesSlice"
 import questions from "./slices/questionsSlice"
-import answers from "./slices/answersSlice"
 
 const reducer = combineReducers({
   categories,
   questions,
-  answers,
 })
 
 const store = configureStore({
