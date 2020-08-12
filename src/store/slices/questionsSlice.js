6275570c00a59a71904ec02351a14e9cd18aea55
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-11 22:04:37 -0300

    feat(slice): add transform question function to create a new array of questions
    - add id to new array
    - add answers to combine correct and incorrect answers
    - add sort method to answers

--- src/store/slices/questionsSlice.js
+++ src/store/slices/questionsSlice.js
@@ -1,15 +1,29 @@
-import { createSlice } from '@reduxjs/toolkit'
-import questionsMock from '../../data/questionsMock';
+import { createSlice } from "@reduxjs/toolkit";
+import questionsMock from "../../data/questionsMock";
+
+const transformQuestions = (questions) => {
+  const transformedQuestions = questions.map((originalQuestion, index) => {
+    const { correct_answer, question, incorrect_answers, ...otherProperties } = originalQuestion;
+
+    const answers = [correct_answer, ...incorrect_answers].sort();
+
+    return {
+      id: index,
+      answers,
+      correctAnswer: correct_answer,
+      description: question,
+      ...otherProperties,
+    };
+  });
+
+  // retornar shuffled transformedQuestions
+  return transformedQuestions;
+};
 
 const questionsSlice = createSlice({
-    name: 'questions',
-    initialState: questionsMock,
-    reducers: {
-        changeFirstCategoryName: (state, action) => {
-            state[0].category = action.payload;
-        },
-    }
+  name: "questions",
+  initialState: transformQuestions(questionsMock),
+  reducers: {},
 });
 
-export const { changeFirstCategoryName } = questionsSlice.actions;
-export default questionsSlice.reducer
+export default questionsSlice.reducer;
