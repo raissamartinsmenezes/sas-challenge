07eef713a467fd8c89bd4ab2ad209900a181f660
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-11 21:57:12 -0300

    feat(answer): add update to answer component
    - add get button styles function
    - add underlay color to touchable highlight component
    - add isSelected condition to borderColor when answer is select

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,34 +1,35 @@
-import React from 'react';
-import { StyleSheet, TouchableHighlight, Text, Dimensions } from 'react-native';
+import React from "react";
+import { StyleSheet, TouchableHighlight, Text, Dimensions } from "react-native";
 
-const Answer = (props) => {
+const Answer = ({ onClick, answer, isSelected }) => {
   return (
-    <TouchableHighlight onPress={props.onClick}>
-        <Text style={styles.button}>{props.answer}</Text>
+    <TouchableHighlight onPress={onClick} underlayColor="none">
+      <Text style={getButtonStyles(isSelected)}>{answer}</Text>
     </TouchableHighlight>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
-    width: Dimensions.get('window').width - 48, 
+const getButtonStyles = (isSelected) =>
+  ({
+    width: Dimensions.get("window").width - 48,
     padding: 19,
     marginBottom: 16,
-    textAlign: 'left',
-    alignSelf: 'center',
-    color: '#000',
+    textAlign: "left",
+    alignSelf: "center",
+    color: "#000",
     fontSize: 14,
     backgroundColor: '#fff', 
     borderRadius: 8,
     shadowColor: "#000",
+    borderWidth: isSelected ? 2 : 0,
+    borderColor: isSelected ? "blue" : "none",
     shadowOffset: {
-	    width: 0,
-	    height: 1,
+      width: 0,
+      height: 1,
     },
     shadowOpacity: 0.22,
     shadowRadius: 2.22,
     elevation: 3,
-    },
-});
+  });
 
 export default Answer;
