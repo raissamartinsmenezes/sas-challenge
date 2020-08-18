5524f0f14cb18387194d0388f14933d0cb9434cb
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-17 23:30:41 -0300

    feat(questions): add actions and async request with react-thunk

--- src/store/slices/questionsSlice.js
+++ src/store/slices/questionsSlice.js
@@ -1,29 +1,75 @@
 import { createSlice } from "@reduxjs/toolkit";
-import questionsMock from "../../data/questionsMock";
+import unescape from 'lodash/unescape';
 
 const transformQuestions = (questions) => {
   const transformedQuestions = questions.map((originalQuestion, index) => {
     const { correct_answer, question, incorrect_answers, ...otherProperties } = originalQuestion;
 
-    const answers = [correct_answer, ...incorrect_answers].sort();
-
+    const escapedAnswers = [correct_answer, ...incorrect_answers].sort();
+    const answers = escapedAnswers.map(unescape)
+    // const answers = escapedAnswers.map((answer) => unescape(answer))
+    const description = unescape(question) 
+    
     return {
       id: index,
       answers,
+      description,
       correctAnswer: correct_answer,
-      description: question,
       ...otherProperties,
     };
   });
 
-  // retornar shuffled transformedQuestions
   return transformedQuestions;
 };
 
+const initialState = {
+  list: [],
+  isLoading: true,
+  error: null
+}
+
 const questionsSlice = createSlice({
   name: "questions",
-  initialState: transformQuestions(questionsMock),
-  reducers: {},
+  initialState,
+  reducers: {
+    getQuestions: (state) => {
+      state.isLoading = true
+      state.error = null
+    },
+    getQuestionsSuccess: (state, actions) => {
+      state.list = transformQuestions(actions.payload)
+      state.isLoading = false
+      state.error = null
+    },
+    getQuestionsFailure: (state, actions) => {
+      state.isLoading = false
+      state.error = actions.payload
+    },
+    resetQuestions: (state) => {
+      state.list = []
+    }
+  },
 });
+
+export const {
+  getQuestions,
+  getQuestionsSuccess,
+  getQuestionsFailure,
+  resetQuestions
+} = questionsSlice.actions
+
+export const fetchQuestions = (categoryId) => async (dispatch) => {
+  
+  dispatch(getQuestions())
+  try {
+    const request = await fetch(
+      `https://opentdb.com/api.php?amount=50&category=${categoryId}&type=multiple`
+    )
+    const requestResult = await request.json()
+    dispatch(getQuestionsSuccess(requestResult.results))
+  } catch(err) {
+    dispatch(getQuestionsFailure(err.toString()))
+  }
+}
 
 export default questionsSlice.reducer;
