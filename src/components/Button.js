f405eb42f5ac69d0d9c2dc60c0eab9c72fbd3b34
raissamartinsmenezes <raissamartinsdemenezes@gmail.com>
2020-08-17 23:43:21 -0300

    feat(button): update button to be reusable

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,30 +3,28 @@ import { StyleSheet, TouchableHighlight, Text, Dimensions } from 'react-native';
 
 const Button = (props) => {
   return (
-    <TouchableHighlight onPress={props.onClick}>
-        <Text style={styles.button}>{props.label}</Text>
+    <TouchableHighlight onPress={props.onClick} underlayColor="none">
+        <Text style={getButtonStyles(props.backgroundColor, props.textColor, props.borderWidth)}>{props.label}</Text>
     </TouchableHighlight>
   );
 };
 
-const styles = StyleSheet.create({
-  button: {
+const getButtonStyles = (backgroundColor, textColor, borderWidth) => ({
     width: Dimensions.get('window').width - 48, 
     paddingVertical: 11,
     paddingHorizontal: 24,
     textAlign: 'center',
     alignSelf: 'center',
-    color: '#78809A',
+    color: textColor,
     fontWeight: 'bold', 
     fontSize: 16,
-    backgroundColor: '#fff', 
+    backgroundColor: backgroundColor, 
     borderRadius: 10,
     borderColor: '#78809A',
-    borderWidth: 1,
+    borderWidth: borderWidth,
     shadowColor: '#ccc',
     shadowOpacity: 0.15,
     shadowOffset: { width: 1, height: 2 },
-  },
 });
 
 export default Button;
